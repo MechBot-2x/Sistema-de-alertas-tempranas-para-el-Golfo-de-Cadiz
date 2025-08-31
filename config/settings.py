e45f57af7af0b94f5168cb6d004257d93f9d6416
IA. MechMind <ia.mechmind@gmail.com>
2025-08-31 23:35:36 +0200

    Update settings.py

---
+++ config/settings.py
@@ -0,0 +1,22 @@
+{
+    "nombre": "Sistema de Alertas Tempranas Golfo de Cádiz",
+    "version": "2.0.0",
+    "organizacion": "MechBot-2x",
+    "garantia": "2025-11-30",
+    "configuracion": {
+        "telegram": {
+            "bot_token": "8478499112:AAGxqzYm4I-3Zyc9XCXIkE3mLOl8pXFOM00",
+            "chat_id": "8350588401"
+        },
+        "api_endpoints": {
+            "ign": "https://www.ign.es/web/ign/portal",
+            "aemet": "https://www.aemet.es/es/portada",
+            "puertos": "https://puertos.es/es-es"
+        },
+        "umbrales": {
+            "tsunami": 6.5,
+            "sismico": 5.0,
+            "meteorologico": 3.0
+        }
+    }
+}
